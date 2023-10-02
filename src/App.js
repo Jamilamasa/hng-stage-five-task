7fa66a2d8898aecdea7ee10f26f56ad4e1ab63c7
JamilAmasa <jamilamasa@gmail.com>
2023-10-02 11:25:08 +0100

    final commit

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from 'react';
 import {
   ChakraProvider,
-  Box,
-  Text,
-  Link,
-  VStack,
-  Code,
-  Grid,
-  extendTheme,
 } from '@chakra-ui/react';
-import { ColorModeSwitcher } from './ColorModeSwitcher';
-import { Logo } from './Logo';
 import Home from './components/Home';
-import ScreenRecorder from './components/media';
-// Supports weights 100-800
-import '@fontsource-variable/sora';
-// Supports weights 100-900
-import '@fontsource-variable/work-sans';
 
-const colors = {
-  fonts: {
-    heading: `'Work Sans Variable', sans-serif`,
-    body: `'Work Sans Variable', sans-serif`,
-   
-  },
-}
-
-const theme = extendTheme({ colors })
 
 function App() {
   return (
-    <ChakraProvider theme={theme}>
+    <ChakraProvider >
       <Home/>
     
     </ChakraProvider>
